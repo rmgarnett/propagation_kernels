65e32b78fe7a0eeb7a0365ce33b6053712313b5a
Roman Garnett <romanempire@gmail.com>
2012-11-21 18:41:53 +0100

    changing argument order in call

--- python/propagation_kernel.py
+++ python/propagation_kernel.py
@@ -3,12 +3,12 @@ from ctypes import *
 
 lib = cdll.LoadLibrary('propagation_kernel.so')
 lib.propagation_kernel.restype  = None
-lib.propagation_kernel.argtypes = [POINTER(c_int),
-                                   POINTER(c_double),
-                                   c_int,
-                                   c_int,
-                                   c_int,
+lib.propagation_kernel.argtypes = [POINTER(c_double),
+                                   POINTER(c_int),
                                    c_double,
+                                   c_int,
+                                   c_int,
+                                   c_int,
                                    c_int,
                                    POINTER(c_double)]
 
@@ -16,21 +16,21 @@ def compute(graph_ind, probabilities, w, p):
 
     num_graphs = int(graph_ind.max())
     num_nodes  = probabilities.shape[0]
-    num_labels = probabilities.shape[1]
+    num_classes = probabilities.shape[1]
 
     K = numpy.zeros((num_graphs, num_graphs), float)
 
     graph_ind     = numpy.asfortranarray(graph_ind,     numpy.intc)
     probabilities = numpy.asfortranarray(probabilities, float)
     K             = numpy.asfortranarray(K)
-    
-    lib.propagation_kernel(graph_ind.ctypes.data_as(POINTER(c_int)),
-                           probabilities.ctypes.data_as(POINTER(c_double)),
-                           num_nodes,
-                           num_labels,
-                           num_graphs,
+
+    lib.propagation_kernel(probabilities.ctypes.data_as(POINTER(c_double)),
+                           graph_ind.ctypes.data_as(POINTER(c_int)),
                            w,
                            int(p),
+                           num_nodes,
+                           num_classes,
+                           num_graphs,
                            K.ctypes.data_as(POINTER(c_double)))
-    
+
     return K
