dc656fbb37416c7f325617df80196ffb4013d41d
rmgarnett <romanempire@gmail.com>
2014-02-11 23:42:06 +0100

    documentation and removing spurious input

--- calculate_hashes.m
+++ calculate_hashes.m
@@ -1,11 +1,61 @@
-function labels = calculate_hashes(features, graph_ind, distance, w)
+% CALCULATE_HASHES compute locality sensitive hashes.
+%
+% This function computes a locality sensitive hash for vectors in the
+% manner described in:
+%
+%   Neumann, M., Patricia, N., Garnett, R., and Kersting, K. Efficient
+%   Graph Kernels by Randomization. (2012). Machine Learning And
+%   Knowledge Discovery in Databases: European Conference, (ECML/PKDD
+%   2012), pp. 378-392.
+%
+% This implmentation supports (approximately) preserving any of the
+% following distances:
+%
+%   - \ell^1 or \ell^2 for arbitrary feature vectors
+%   - the total variation or Hellinger distance for
+%     distribution-valued features
+%
+% Depending on the chosen distance, the input features will be
+% transformed appropriately.
+%
+% Usage:
+%
+%   labels = calculate_hashes(features, graph_ind, distance, w)
+%
+% Inputs:
+%
+%    features: an (n x d) matrix of feature vectors to hash. If either
+%              the total variation or Helligner distances is chosen,
+%              each row of this matrix should sum to 1.
+%    distance: a string indicating the distance; the following
+%              values are supported:
+%
+%                'l1': \ell^1
+%                'l2': \ell^2
+%                'tv': total variation distnace (equivalent to \ell^1)
+%         'hellinger': Hellinger distance
+%
+%              The input is not case sensitive.
+%
+%           w: the bin width
+%
+% Outputs:
+%
+%   labels: an (n x 1) vector containing the hashed features. The
+%           hashes are guaranteed to be positive integers in a
+%           contiguous range starting from 1.
+%
+% See also PROPAGATION_KERNEL.
+
+% Copyright (c) Roman Garnett, 2012--2014.
+
+function labels = calculate_hashes(features, distance, w)
 
   % determine path to take depending on chosen distance
   use_cauchy = (strcmpi(distance, 'l1') || strcmpi(distance, 'tv'));
   take_sqrt  = strcmpi(distance, 'hellinger');
 
   [num_points, d] = size(features);
-  num_graphs = max(graph_ind);
 
   if (take_sqrt)
     features = sqrt(features);
@@ -23,6 +73,8 @@ function labels = calculate_hashes(features, graph_ind, distance, w)
 
   % compute hashes
   labels = floor((features * v + b) * (1 / w));
+
+  % map to contiguous integers starting from 1
   [~, ~, labels] = unique(labels);
 
 end
